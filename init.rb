411b2ce4a955915ab0526623c31c34b039faac24
Adam Lantos <adam.lantos@playma.org>
2010-09-17 14:15:31 +0200

    Add settings (enable, url and frequency).

--- init.rb
+++ init.rb
@@ -7,4 +7,10 @@ Redmine::Plugin.register :redmine_session_ping do
   author 'Adam Lantos'
   description 'Javascript-based session pinging plugin.'
   version '0.0.1'
+  settings :partial => 'settings/settings',
+    :default => {
+      'enabled' => 'false',
+      'url' => '/favicon.ico',
+      'frequency' => '300'
+    }
 end
